d002fd7bb3e0deb3568030113024314e170def76
Kaan Ismet Okul <Kaanismet7@gmail.com>
2022-09-29 22:28:37 -0400

    first commit

---
+++ src/test/java/com/KaanOkul/OnlineBankingSystem/OnlineBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KaanOkul.OnlineBankingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
